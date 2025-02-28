2f4c479291ea6f45b74a6398876a3f3ff816252c
[yesha vyas] <[yeshavyas27@gmail.com]>
2025-02-28 12:28:13 +0530

    Extracted specific data from job pages in dictionary

--- experiments/web_scraping_naukri_playwright.py
+++ experiments/web_scraping_naukri_playwright.py
@@ -2,23 +2,29 @@ from playwright.async_api import async_playwright
 
 class ScrapeNaukriService():
     def __init__(self, **kwargs):
+        self.start_url = self.__generate_url(kwargs)
+        print(self.start_url)
+        
+    def __generate_url(self, kwargs):
         skills_designations_companies = kwargs["skills_designations_companies"]
         location = kwargs["location"]
         experience = kwargs["experience"]
         page = skills_designations_companies.replace(" ", "-").replace(",", "-") + "-jobs"
+        if kwargs["page_number"] > 1:
+            page += f"-{kwargs["page_number"]}"
 
         key_skills_designations_companies = skills_designations_companies.replace(" ", "%20").replace(",", "%2C")
-        self.start_url = f"https://www.naukri.com/{page}?k={key_skills_designations_companies}"
+        start_url = f"https://www.naukri.com/{page}?k={key_skills_designations_companies}"
         if location:
-            self.start_url += "-in-" + location.split()[0]
+            start_url += "-in-" + location.split()[0]
             key_location = location.replace(" ", "%20").replace(",", "%2C")
-            self.start_url += f"&l={key_location}"
+            start_url += f"&l={key_location}"
         if experience:
-            self.start_url += f"&experience={experience}"
+            start_url += f"&experience={experience}"
         if location or experience:
-            self.start_url += f"&nignbevent_src=jobsearchDeskGNB"
-        # TODO: add page query to url as well
-        print(self.start_url)
+            start_url += f"&nignbevent_src=jobsearchDeskGNB"
+        
+        return start_url
 
     async def scrape(self):
         async with async_playwright() as p:
@@ -27,36 +33,72 @@ class ScrapeNaukriService():
             context = await browser.new_context(**chrome_device)
             page = await context.new_page()
             await page.goto(self.start_url)  
-            TIMEOUT = 5000
+            TIMEOUT = 6000
             await page.wait_for_load_state("networkidle", timeout=TIMEOUT)
-            
+            jobs = []
             for job in await page.locator("css=div.styles_jlc__main__VdwtF > div.srp-jobtuple-wrapper").all():
                 job_link = await job.locator("css=a.title").get_attribute("href")
-                print(job_link)
                 job_page = await context.new_page()
                 await job_page.goto(job_link)
                 await job_page.wait_for_load_state("networkidle", timeout=TIMEOUT)
-                # get job title, company, location - section.styles_job-header-container___0wLZ
-                print("Job header:")
-                print(await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[1]/header/h1").text_content())
-                # job description,skills, education - section.styles_job-desc-container__txpYf
-                print("Job description:")
-                print(await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[2]").text_content())
+                job_title = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[1]/header/h1").get_attribute("title")
+                company_name = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[1]/div/a").text_content()
+                years_of_experience = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[2]/div[1]/div[1]/span").text_content()
+                salary = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[2]/div[1]/div[2]/span").text_content()
+                location = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[1]/div[1]/div[2]/div[2]/span").text_content()
                 
+                job_description = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[2]").text_content()
+                education = await job_page.locator("xpath=/html/body/div/div/main/div[1]/div[1]/section[2]/div[2]/div[3]/div[2]").text_content()
 
-                # returns job link, company name, job description, job name, key skills, education required, number of years of experience required
-                # just print single job description and stop for experiment purpose
+                skills_container =  job_page.locator("css=div.styles_key-skill__GIPn_")
+                all_skills = await skills_container.locator("a").all_text_contents()
+                skill_elements = await skills_container.locator("a").all()
+
+                key_skills = []
+                for skill in skill_elements:
+                    if await skill.locator("i.ni-icon-jd-save").count() > 0:
+                        key_skills.append(await skill.locator("span").text_content())
+
+                if not key_skills:
+                    key_skills = all_skills
+
+                jobs.append({
+                    "job_link": job_link,
+                    "company_name": company_name,
+                    "job_title": job_title,
+                    "years_of_experience": years_of_experience,
+                    "salary": salary,
+                    "location": location,
+                    "job_description": job_description,
+                    "education": education,
+                    "all_skills": all_skills,
+                    "key_skills": key_skills
+                })
                 
-                break
-                print("------------------------------------------------------")
 
             await browser.close()
+            return jobs
+
+            
 
 import asyncio
 
-asyncio.run(ScrapeNaukriService(
+jobs = asyncio.run(ScrapeNaukriService(
     skills_designations_companies="software dev", 
-    experience=None,
+    experience=1,
     location=None,
-    page=None
+    page_number=1
 ).scrape())
+
+print(jobs)
+'''
+job_title
+years of experience
+comapnay name
+job link
+location
+salary
+job description
+key skills
+education
+'''
