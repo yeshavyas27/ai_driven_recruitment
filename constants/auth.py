3e8f87e93e1004e9c8de2c56beae23ed3eb31214
yeshavyas27 <yeshavyas27@gmail.com>
2025-04-20 11:44:20 +0530

    Access token lifetime increased to 1 day

--- constants/auth.py
+++ constants/auth.py
@@ -5,7 +5,7 @@ class AuthConstants:
 
     SECRET_KEY: Final = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
     ALGORITHM: Final = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: Final = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: Final = 1440  # 1 day
 
 class UserRoles:
     RECRUITER: Final = "recruiter"
